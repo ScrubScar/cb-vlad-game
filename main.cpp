70eccd7c2ac0ee434715067f9616f72f745a81c7
Adam Sarabia <asarabiag@gmail.com>
2009-01-06 14:34:50 -0800

    Forgot main file in initial commit.

---
+++ main.cpp
@@ -0,0 +1,39 @@
+
+#include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
+#include "SDL/SDL_ttf.h"
+#include <string>
+#include "MapOne.h"
+#include "engine.h"
+
+using namespace std;
+
+int main( int argc, char* args[] )
+{
+    bool quit = false;
+    SDL_Event event;
+    engine Game;
+    Game.init(640, 480);
+
+
+    Uint8 *keystates = SDL_GetKeyState(NULL);
+    while( quit == false ) {
+           while( SDL_PollEvent( &event ) )
+           {
+                  if( event.type == SDL_QUIT )
+                  {
+                      quit = true;
+                      }
+
+                   if( keystates[SDLK_ESCAPE])
+                   {
+                       quit = true;
+                       }
+                  }
+
+                  Game.heartBeat(event);
+           }
+    return 0;
+}
+
+
